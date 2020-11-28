66be3670313fe3da57954ce06eaf0384b2282625
huangyouyou <you-you.huang@microfocus.com>
2020-11-28 23:06:17 +0800

    spiralOrder

---
+++ src/main/java/com/yy/leetcode/array/spiralOrderTest.java
@@ -0,0 +1,14 @@
+package com.yy.leetcode.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class spiralOrderTest {
+    spiralOrder spiralOrder = new spiralOrder();
+
+    @Test
+    public void spiralOrder() {
+        spiralOrder.spiralOrder(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+    }
+}
