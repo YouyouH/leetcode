cd681bdd0854937f1092affdbe4ebb3b9e9410bf
huangyouyou <you-you.huang@microfocus.com>
2021-03-28 19:52:18 +0800

    remove-duplicates-from-sorted-list-ii.go

---
+++ src/main/java/com/yy/leetcode/linkedlist/remove-duplicates-from-sorted-list-ii.go
@@ -0,0 +1,39 @@
+package linkedlist
+
+/**
+存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除链表中所有存在数字重复情况的节点，只保留原始链表中 没有重复出现 的数字。
+
+返回同样按升序排列的结果链表。
+
+
+示例 1：
+
+输入：head = [1,2,3,3,4,4,5]
+输出：[1,2,5]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+func deleteDuplicates(head *ListNode) *ListNode {
+
+	if head == nil {
+		return head
+	}
+	dummy := &ListNode{0, head}
+
+	cur := dummy
+
+	for cur.Next != nil && cur.Next.Next != nil {
+		if cur.Next.Val == cur.Next.Next.Val {
+			val := cur.Next.Val
+			for cur.Next != nil && cur.Next.Val == val {
+				cur.Next = cur.Next.Next
+			}
+		} else {
+			cur = cur.Next
+		}
+	}
+	return dummy.Next
+
+}
