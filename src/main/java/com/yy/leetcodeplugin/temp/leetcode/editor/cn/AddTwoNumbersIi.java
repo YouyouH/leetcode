a4317aee61a620a26d1bc4afcff3d05d4dd21fb3
you-you.huang <you-you.huang@microfocus.com>
2025-02-14 22:29:23 +0800

    二叉树的前序遍历

---
+++ src/main/java/com/yy/leetcodeplugin/temp/leetcode/editor/cn/AddTwoNumbersIi.java
@@ -0,0 +1,75 @@
+
+  
+package com.yy.leetcodeplugin.temp.leetcode.editor.cn;
+
+//2025-02-12 22:45:37 
+//两数相加 II
+
+public class AddTwoNumbersIi{
+    public static void main(String[] args) {
+         Solution solution = new AddTwoNumbersIi().new Solution();
+    }
+        //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+  //给你两个 非空 链表来代表两个非负整数。数字最高位位于链表开始位置。它们的每个节点只存储一位数字。将这两数相加会返回一个新的链表。 
+//
+// 你可以假设除了数字 0 之外，这两个数字都不会以零开头。 
+//
+// 
+//
+// 示例1： 
+//
+// 
+//
+// 
+//输入：l1 = [7,2,4,3], l2 = [5,6,4]
+//输出：[7,8,0,7]
+// 
+//
+// 示例2： 
+//
+// 
+//输入：l1 = [2,4,3], l2 = [5,6,4]
+//输出：[8,0,7]
+// 
+//
+// 示例3： 
+//
+// 
+//输入：l1 = [0], l2 = [0]
+//输出：[0]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表的长度范围为 [1, 100] 
+// 0 <= node.val <= 9 
+// 输入数据保证链表代表的数字无前导 0 
+// 
+//
+// 
+//
+// 进阶：如果输入链表不能翻转该如何解决？ 
+//
+// Related Topics 栈 链表 数学 👍 752 👎 0
