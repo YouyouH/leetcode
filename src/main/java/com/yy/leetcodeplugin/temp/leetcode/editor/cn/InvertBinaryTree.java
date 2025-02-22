220ff202867f30a5c2bbbde905bd24458bd148c4
you-you.huang <you-you.huang@microfocus.com>
2025-02-22 22:51:42 +0800

    翻转二叉树

---
+++ src/main/java/com/yy/leetcodeplugin/temp/leetcode/editor/cn/InvertBinaryTree.java
@@ -0,0 +1,92 @@
+
+  
+package com.yy.leetcodeplugin.temp.leetcode.editor.cn;
+
+//2025-02-22 22:42:04 
+//翻转二叉树
+
+public class InvertBinaryTree{
+    public static void main(String[] args) {
+         Solution solution = new InvertBinaryTree().new Solution();
+    }
+        //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+
+        if (root == null) {
+            return null;
+        }
+
+        if (root.left == root.right) {
+            return root;
+        }
+
+        /**
+         * FIXME 下面的写法是错误的
+         *  root.left第一行修改之后第二行不能再用了，所以要先赋值避免干扰
+         */
+        TreeNode left = invertTree(root.right);
+        TreeNode right = invertTree(root.left);
+        root.left = left;
+        root.right = right;
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+  //给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：root = [4,2,7,1,3,6,9]
+//输出：[4,7,2,9,6,3,1]
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：root = [2,1,3]
+//输出：[2,3,1]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目范围在 [0, 100] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1915 👎 0
